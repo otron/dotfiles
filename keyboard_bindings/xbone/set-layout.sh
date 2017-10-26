94a7c69e479280c8d3fd6ee7a2035ffd868eb1c0
Odd <oddweb42+gith@gmail.com>
2017-10-26 11:17:10 +0200

    add binds to swap alt/windows keys on xbone

--- keyboard_bindings/xbone/set-layout.sh
+++ keyboard_bindings/xbone/set-layout.sh
@@ -5,3 +5,9 @@ setxkbmap us -option "caps:escape" -variant mac -option "lv3:alt_switch"
 
 # remap print screen to super_r
 xmodmap -e "keycode 107 = Super_R"
+
+# when ergodox/external keyboard isn't available, swap alt and super/windows keys
+xmodmap -e "keycode 133 = ISO_Level3_Shift"
+xmodmap -e "keycode 64 = Super_L"
+xmodmap -e "keycode 108 = Super_R"
+xmodmap -e "keycode 107 = ISO_Level3_Shift"
